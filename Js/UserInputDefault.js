44eab4876948a16a104ce45cfbfd1c3a86262014
eliab256 <bordonielia96@gmail.com>
2024-08-20 18:29:53 +0200

    aggiunta cartella js e creazione input js

---
+++ Js/UserInputDefault.js
@@ -0,0 +1,7 @@
+
+let nameVariable;
+
+document.getElementById("nameId").onclick = function(){
+
+    nameVariable = document.getElementById("textId").value; //ora il valore scritto in input viene assegnato alla variabile
+}
